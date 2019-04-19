1341ecd6eab8fd92596d92375a1c49e3b3a76a38
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-04-19 13:29:45 -0700

    Created context and some classes along with the MemberDb

---
+++ FirstTimeDadSite/Models/SiteContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstTimeDadSite.Models
+{
+    public class SiteContext : DbContext
+    {
+        public SiteContext(DbContextOptions<SiteContext> options) : base(options)
+        {
+
+        }
+
+        //Add all models (classes) as DbSet that need to be tracked by the database
+        public DbSet<Member> Members { get; set; }
+
+        public DbSet<Tips> Tips { get; set; }
+    }
+}
